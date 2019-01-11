416feb969e84a1348356b56e27a4100f8a23565a
Vishal Desai <vddesai1998@gmail.com>
2019-01-11 19:28:50 +0530

    Add redis and start communicating with hydrus server

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import socketio
+import urllib.request
+import redis
+import json
 
 sio = socketio.Client(logger=True)
 
@@ -13,8 +16,23 @@ class MyCustomNamespace(socketio.ClientNamespace):
         self.emit('my_response', data)
 
     def on_update(self, data):
+        print("UPDATED: ")
         print(data)
 
 sio.register_namespace(MyCustomNamespace('/test'))
 sio.connect("http://localhost:8080/serverapi")
 sio.emit("my_event", {"message": "Hey, Hii"})
+con = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+def get_objects(url):
+    resp = urllib.request.urlopen(url)
+    data = json.loads(resp.read().decode('utf-8'))
+    print("Cached ")
+    print(data)
+    con.set("cache", json.dumps(data))
+    d = con.get("cache").decode('utf-8')
+    print("Got ")
+    print(d)
+
+
+get_objects("http://localhost:8080/serverapi/DroneCollection")
