76cfb1a4135bde2cc569652863ef3cd4f75513cc
vddesai1871 <vddesai1998@gmail.com>
2019-01-18 12:34:30 +0530

    Reconfigure namespace

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import json
 
 sio = socketio.Client(logger=True)
 
+
 class MyCustomNamespace(socketio.ClientNamespace):
     def on_connect(self):
         pass
@@ -12,27 +13,30 @@ class MyCustomNamespace(socketio.ClientNamespace):
     def on_disconnect(self):
         pass
 
-    def on_my_event(self, data):
-        self.emit('my_response', data)
-
     def on_update(self, data):
         print("UPDATED: ")
         print(data)
 
+    def on_my_response(self, data):
+        print("Got response")
+        self.emit('resp', data)
+
+
 sio.register_namespace(MyCustomNamespace('/test'))
 sio.connect("http://localhost:8080/serverapi")
-sio.emit("my_event", {"message": "Hey, Hii"})
 con = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 def get_objects(url):
     resp = urllib.request.urlopen(url)
+    print(resp)
     data = json.loads(resp.read().decode('utf-8'))
     print("Cached ")
     print(data)
     con.set("cache", json.dumps(data))
     d = con.get("cache").decode('utf-8')
-    print("Got ")
+    print("Got from cache ")
     print(d)
 
 
 get_objects("http://localhost:8080/serverapi/DroneCollection")
+sio.emit("my_event", {"message": "Hey, Hii"}, namespace="/test")
