278a935bab56af739eb52fad721bfc5b4394bc66
vddesai1871 <vddesai1998@gmail.com>
2019-07-23 14:55:12 +0530

    Update with work at hydrus.

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ def update_obj(id, obj):
 
 class MyCustomNamespace(socketio.ClientNamespace):
     def on_connect(self):
-        pass
+        print("Connected")
 
     def on_disconnect(self):
         pass
@@ -20,41 +20,16 @@ class MyCustomNamespace(socketio.ClientNamespace):
     def on_update(self, data):
         print("UPDATED: ")
         print("ID")
-        print(data["id"])
-        print(data["obj"])
-        update_obj(data["id"], data["obj"])
-
+        print(data["new_job_id"])
+        print(data["last_job_id"])
+        print(data["method"])
+        print(data["resource_url"])
 
     def on_my_response(self, data):
         print("Got response")
         self.emit('resp', data)
 
 
-sio.register_namespace(MyCustomNamespace('/test'))
+sio.register_namespace(MyCustomNamespace('/sync'))
 sio.connect("http://localhost:8080/serverapi")
-con = redis.StrictRedis(host="localhost", port=6379, db=0)
-
-def get_objects(url):
-    resp = urllib.request.urlopen(url)
-    print(resp)
-    data = json.loads(resp.read().decode('utf-8'))
-    print("Cached ")
-    print(data)
-    for obj in data["members"]:
-        id = obj["@id"].split('/')[-1]
-        obj_type = obj["@type"]
-        res = urllib.request.urlopen(url+"/"+id)
-        member_obj = json.loads(res.read().decode('utf-8'))
-        member_obj.pop("@context")
-        member_obj.pop("@id")
-        con.set(obj_type+":"+id, json.dumps(member_obj))
-        tmp = con.get(obj_type+":"+id).decode('utf-8')
-        print(tmp)
-    con.set("cache", json.dumps(data))
-    d = con.get("cache").decode('utf-8')
-    print("Got from cache ")
-    print(d)
-
-
-get_objects("http://localhost:8080/serverapi/DroneCollection")
-sio.emit("my_event", {"message": "Hey, Hii"}, namespace="/test")
+sio.emit("my_event", {"message": "Hey, Hii"}, namespace="/sync")
