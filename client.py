76f51f15e03dc5814f9345b128bd447164fd63ba
vddesai1871 <vddesai1998@gmail.com>
2019-07-30 17:23:20 +0530

    Update with hydrus.

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socketio
 import urllib.request
 import redis
 import json
+import time
 
 sio = socketio.Client(logger=True)
 
@@ -19,17 +20,18 @@ class MyCustomNamespace(socketio.ClientNamespace):
 
     def on_update(self, data):
         print("UPDATED: ")
-        print("ID")
-        print(data["new_job_id"])
-        print(data["last_job_id"])
-        print(data["method"])
-        print(data["resource_url"])
 
     def on_my_response(self, data):
         print("Got response")
         self.emit('resp', data)
+    
+    def on_modification_table_diff(self, data):
+        print("Modification Table Diff:")
+        print(data)
 
 
 sio.register_namespace(MyCustomNamespace('/sync'))
 sio.connect("http://localhost:8080/serverapi")
 sio.emit("my_event", {"message": "Hey, Hii"}, namespace="/sync")
+time.sleep(10)
+sio.emit("get_modification_table_diff", {"agent_job_id": "7bb5c816-4436-4ee8-8aaf-36017ae16e59"}, namespace = "/sync")
